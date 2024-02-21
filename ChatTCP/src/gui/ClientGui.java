75b123f90bd345c0e46ec752535f7949c1924035
aitor <aitorvadillo70@gmail.com>
2024-02-21 18:36:32 +0100

    onActionListener fixed

--- ChatTCP/src/gui/ClientGui.java
+++ ChatTCP/src/gui/ClientGui.java
@@ -1,5 +1,6 @@
 package gui;
 
+import utils.client.Client;
 import utils.server.ClientHandler;
 import utils.server.ServerMain;
 
@@ -7,15 +8,16 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class ClientGui extends JFrame{
+public class ClientGui extends JFrame {
     private String clientName;
+    private Client client;
     private JPanel panel1;
     private JButton sendButton;
     private JScrollPane msgScrollPane;
     private JTextArea msgTextArea;
     private JScrollPane textScrollPanel;
 
-    public ClientGui(String clientName) {
+    public ClientGui(String clientName, Client client) {
         setSize(420, 420);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
@@ -23,11 +25,14 @@ public class ClientGui extends JFrame{
         add(panel1);
         setLocationRelativeTo(null);
         this.clientName = clientName;
+        this.client = client;
 
         sendButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                
+                String msg = msgTextArea.getText();
+                client.sendMessage(msg);
+                msgTextArea.setText("");
             }
         });
     }
